c0210c20874123c1d4749638b73da0d82f1efd75
rjjq <rrrjjjqqq@gmail.com>
2022-08-09 20:41:13 +0800

    Initial Commit

---
+++ autoLayoutPractices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayoutPractices
+//
+//  Created by rjjq on 2022/8/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
